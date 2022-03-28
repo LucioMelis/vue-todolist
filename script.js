6380351615074f5a3fe40dedb6f4412a76a41cd4
luciomelis <luciomelis1992@gmail.com>
2022-03-28 15:27:00 +0200

    tutto pronto

--- script.js
+++ script.js
@@ -1,2 +1,64 @@
 console.log('Js Ok');
 
+const list = [
+    {
+        text: 'Schierare la formazione al Fantacalcio',
+        done: true,
+    },
+    {
+        text: 'Studiare Vue Js',
+        done: true,
+    },
+    {
+        text: 'Fare la Lavatrice',
+        done: true,
+    },
+    {
+        text: 'Fare la spesa',
+        done: true,
+    },
+    {
+        text: 'Pagare Sky',
+        done: true,
+    },
+    {
+        text: 'Guardare il Trono di Spade',
+        done: false,
+    },
+    {
+        text: 'Giocare a Call of Duty',
+        done: false,
+    },
+    {
+        text: 'Andare al Kartodromo',
+        done: false,
+    }
+];
+
+//MILESTONE 1
+//Stampare all'interno di una lista, un item per ogni todo.
+//Se la proprietà done è uguale a true,
+//visualizzare il testo del todo sbarrato.
+
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una "x":
+// cliccando su di essa, il todo viene rimosso dalla lista.
+
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante "aggiungi":
+// cliccando sul pulsante,
+// il testo digitato viene letto e utilizzato per creare un nuovo todo,
+// che quindi viene aggiunto alla lista dei todo esistenti.
+
+
+const app = new Vue({
+    el: '#root',
+    //*********************** DATA ******************/ 
+    data: {
+        list,
+    },
+    //*********************** METHODS ******************/ 
+    methods: {
+
+    }
+});
