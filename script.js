9be8a778a354812945aef1bf7fb51d43b0d5f34d
luciomelis <luciomelis1992@gmail.com>
2022-03-28 21:31:17 +0200

    Finish

--- script.js
+++ script.js
@@ -38,17 +38,26 @@ const list = [
 //MILESTONE 1
 //Stampare all'interno di una lista, un item per ogni todo.
 //Se la proprietà done è uguale a true,
-//visualizzare il testo del todo sbarrato. FATTO!!
+//visualizzare il testo del todo sbarrato. --> FATTO!!
 
 // MILESTONE 2
 // Visualizzare a fianco ad ogni item ha una "x":
-// cliccando su di essa, il todo viene rimosso dalla lista.
+// cliccando su di essa, il todo viene rimosso dalla lista. --> FATTO
 
 // MILESTONE 3
 // Predisporre un campo di input testuale e un pulsante "aggiungi":
 // cliccando sul pulsante,
 // il testo digitato viene letto e utilizzato per creare un nuovo todo,
-// che quindi viene aggiunto alla lista dei todo esistenti.
+// che quindi viene aggiunto alla lista dei todo esistenti. --> FATTO
+
+
+// 1 BONUS- oltre al click sul pulsante,
+// intercettare anche il tasto ENTER
+//  per aggiungere il todo alla lista --> FATTO
+
+// 2 BONUS- cliccando sul testo dell'item,
+// invertire il valore della proprietà done del todo corrispondente 
+// (se done era uguale a false, impostare true e viceversa)
 
 
 const app = new Vue({
@@ -62,7 +71,7 @@ const app = new Vue({
     methods: {
         aggiungiClasse(elementiDone) {
             if (elementiDone === true) {
-                return 'text-decoration-line-through'
+                return 'text-decoration-line-through text-danger'
             }
         },
 
@@ -83,6 +92,14 @@ const app = new Vue({
                 this.valoreInput = '';
                 this.list.push(nuovoOggetto);
                 console.log(nuovoOggetto);
+            }
+        },
+        cambioBooleana(indice) {
+
+            if (this.list[indice].done === false) {
+                this.list[indice].done = true;
+            } else {
+                this.list[indice].done = false;
             }
         }
     }
